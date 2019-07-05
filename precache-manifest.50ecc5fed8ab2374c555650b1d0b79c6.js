efae2e3dec26890c9988fe9ef9cf07fd161b034a
Vinoth Pandian <pandian@fit.fraunhofer.de>
2019-07-06 01:46:26 +0200

    Updates

---
+++ precache-manifest.50ecc5fed8ab2374c555650b1d0b79c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688c55c2866966b9b99a28e2c45bfd9f",
+    "url": "/memberries/index.html"
+  },
+  {
+    "revision": "2946332e8b9a3c94ce29",
+    "url": "/memberries/static/css/main.f5b0732c.chunk.css"
+  },
+  {
+    "revision": "27fb3acfb828e326b825",
+    "url": "/memberries/static/js/2.38a9af76.chunk.js"
+  },
+  {
+    "revision": "2946332e8b9a3c94ce29",
+    "url": "/memberries/static/js/main.ca81501a.chunk.js"
+  },
+  {
+    "revision": "b8e1b1cbe6d675c5f25a",
+    "url": "/memberries/static/js/runtime~main.988c1a7e.js"
+  }
+]);
